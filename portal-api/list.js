a65fc2a064aef3c54213b64e87f7b4e5a47bc8d1
jalehoux <jalehoux@gmail.com>
2018-11-30 16:45:04 -0500

    basic loaded in

---
+++ portal-api/list.js
@@ -0,0 +1,20 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: "surveys",
+    KeyConditionExpression: "userid = :userid",
+    ExpressionAttributeValues: {
+      ":userid": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    return failure({ status: e });
+  }
+}
